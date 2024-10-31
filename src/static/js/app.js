ba60886cec1d231b563b10899aabe3f6d92eb858
JohnnyG33k <148485696+JohnnyG33k@users.noreply.github.com>
2024-10-31 16:37:51 +0100

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Votre panier est vide, ajoutez-y un article !</p>
+                <p className="text-center">Votre panier est vide, ajoutez-y plein d'articles !</p>
             )}
             {items.map(item => (
                 <ItemDisplay
